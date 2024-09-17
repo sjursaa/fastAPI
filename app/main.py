3e85c23f93519f34472798b98a982ba96af94f26
Sjur <sjursaa@gmail.com>
2024-09-17 16:42:56 +0200

    added uvicorn to requirements

--- app/main.py
+++ app/main.py
@@ -4,9 +4,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return { "Hello": "World"}
+async def read_root():
+    return { "Message": "Hello World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
